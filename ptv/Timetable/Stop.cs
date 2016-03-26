a5eec14c5fc81c0628dc38a3285cfa4faf145282
Jackson Ming Hu <huming2207@gmail.com>
2016-03-26 23:15:14 +1100

    Correct an case-sensitive issue
    Some settings was very strange in GitHub desktop...

---
+++ ptv/Timetable/Stop.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ptv.Timetable
+{
+    [JsonObject()]
+    public class Stop : Location
+    {
+        [JsonProperty(PropertyName = "transport_type")]
+        public TransportType TransportType { get; set; }
+
+        [JsonProperty(PropertyName = "stop_id")]
+        public string StopID { get; set; }
+    }
+}
